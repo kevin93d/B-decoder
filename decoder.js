d8773b5898f6828e909c62ffc6c521a195ea1b4d
Yufeng Ding <aimixiu1993@gmail.com>
2019-06-05 22:40:28 -0400

    add function to convert list and dictionary

--- decoder.js
+++ decoder.js
@@ -1,18 +1,78 @@
+let pointer = 0;
+
 function integerDecode(str) {
   // data format: i<integer>e
-  const endPos = str.indexOf('e');
-  const integerStr = str.slice(1, endPos);
+  const currentStr = str.slice(pointer);
+  const endPos = currentStr.indexOf('e');
+  const integerStr = currentStr.slice(1, endPos);
 
+  pointer += 1 + endPos;
   return parseInt(integerStr, 10);
 }
 
 function stringDecode(str) {
   // data format: <length>:<string>
-  const delimiterPos = str.indexOf(':');
-  const stringLength = parseInt(str.slice(0, delimiterPos), 10);
+  const currentStr = str.slice(pointer);
+  const delimiterPos = currentStr.indexOf(':');
+  const stringLength = parseInt(str.slice(pointer, pointer + delimiterPos), 10);
+  pointer += delimiterPos + 1 + stringLength;
 
-  return str.slice(delimiterPos + 1, delimiterPos + stringLength + 1);
+  return currentStr.slice(delimiterPos + 1, delimiterPos + stringLength + 1);
 }
 
-// console.log(integerDecode('i66e'));
-console.log(stringDecode('6:string'));
+function listDecode(str) {
+  // data format: l<contents>e
+  const list = [];
+  pointer++;
+
+  while (str[pointer] !== 'e') {
+    list.push(dataStream(str));
+  }
+
+  pointer++;
+  return list;
+}
+
+function dictionaryDecode(str) {
+  // data format: d<contents>e
+  const dictionary = {};
+  pointer++;
+
+  while (str[pointer] !== 'e') {
+    dictionary[stringDecode(str)] = dataStream(str);
+  }
+
+  pointer++;
+  return dictionary;
+}
+
+function dataStream(str) {
+
+  switch (str[pointer]) {
+    case 'i':
+      return integerDecode(str);
+    case 'l':
+      return listDecode(str);
+    case 'd':
+      return dictionaryDecode(str);
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+      return stringDecode(str);
+  }
+}
+
+// console.log(dataStream('i66e'));
+// console.log(dataStream('i-11e'));
+// console.log(dataStream('6:string'));
+// console.log(dataStream('l4:spami42ee'));
+// console.log(dataStream('d3:bar4:spam3:fooi42ee'));
+// console.log(dataStream('l4:spami42ed3:bar4:spam3:fooi42eee'));
+console.log(dataStream('d3:barl4:spami42ee3:fooi42ee'));
